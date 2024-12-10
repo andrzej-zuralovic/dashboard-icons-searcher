fa22ede06c3514cb54d7b970985fb92c327f5ec4
Andzej Zuralovic <andzej.zuralovic@gmail.com>
2024-12-10 23:44:17 +0200

    Scale images more natural and center title

--- src/lib/components/ImageCard.svelte
+++ src/lib/components/ImageCard.svelte
@@ -11,26 +11,32 @@
 </script>
 
 <div
-	class="group flex flex-col items-center size-full justify-center bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 text-gray-900 dark:text-white"
+	class="group relative flex items-center justify-center bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 text-gray-900 dark:text-white overflow-hidden w-full h-full"
 >
-	<div class="mb-2 text-xs invisible group-hover:visible">{image.title}</div>
-	<img class="size-12" src={image.png || image.svg} alt={image.title} />
-	<div class="mt-2 invisible flex justify-around w-full text-xs group-hover:visible">
-		<button
-			class="disabled:text-gray-400"
-			disabled={!image.png}
-			on:click={copyLink(image.png, 'PNG')}
-			title={!image.png ? 'PNG not available' : 'Copy PNG link to clipboard'}
-		>
-			PNG
-		</button>
-		<button
-			class="disabled:text-gray-400"
-			disabled={!image.svg}
-			on:click={copyLink(image.svg, 'SVG')}
-			title={!image.svg ? 'SVG not available' : 'Copy SVG link to clipboard'}
-		>
-			SVG
-		</button>
+	<div class="m-3">
+		<img class="object-contain" src={image.png || image.svg} alt={image.title} />
+	</div>
+	<div
+		class="absolute inset-0 bg-gray-800 bg-opacity-75 flex flex-col justify-center items-center text-center text-white opacity-0 group-hover:opacity-100 transition-opacity font-bold"
+	>
+		<div class="mb-2 text-xs">{image.title}</div>
+		<div class="mt-2 flex justify-around w-full text-xs">
+			<button
+				class="disabled:text-gray-400"
+				disabled={!image.png}
+				on:click={copyLink(image.png, 'PNG')}
+				title={!image.png ? 'PNG not available' : 'Copy PNG link to clipboard'}
+			>
+				PNG
+			</button>
+			<button
+				class="disabled:text-gray-400"
+				disabled={!image.svg}
+				on:click={copyLink(image.svg, 'SVG')}
+				title={!image.svg ? 'SVG not available' : 'Copy SVG link to clipboard'}
+			>
+				SVG
+			</button>
+		</div>
 	</div>
 </div>
